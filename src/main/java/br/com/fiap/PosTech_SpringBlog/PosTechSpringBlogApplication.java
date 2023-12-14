71bf6d9394bcac23a45f6a8074f101efbe5c664e
brunoguerra <brunoguerra@hotmail.com>
2023-12-14 08:37:06 -0300

    first commit

---
+++ src/main/java/br/com/fiap/PosTech_SpringBlog/PosTechSpringBlogApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.PosTech_SpringBlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PosTechSpringBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PosTechSpringBlogApplication.class, args);
+	}
+
+}
