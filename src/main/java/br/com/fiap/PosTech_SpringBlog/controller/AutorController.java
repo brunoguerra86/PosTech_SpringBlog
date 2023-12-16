df61aa07e4093d55589ce21d4e38da34d003a366
brunoguerra <brunoguerra@hotmail.com>
2023-12-16 18:20:17 -0300

    feat: AutorController

--- src/main/java/br/com/fiap/PosTech_SpringBlog/controller/AutorController.java
+++ src/main/java/br/com/fiap/PosTech_SpringBlog/controller/AutorController.java
@@ -1,19 +1,23 @@
 package br.com.fiap.PosTech_SpringBlog.controller;
 
 import br.com.fiap.PosTech_SpringBlog.model.Autor;
+import br.com.fiap.PosTech_SpringBlog.service.AutorService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/autores")
 public class AutorController {
 
+    @Autowired
+    private AutorService autorService;
     @PostMapping
     public Autor criar(@RequestBody Autor autor){
-        return null;
+        return this.autorService.criar(autor);
     }
 
     @GetMapping("/{codigo}")
     public Autor obterPorCodigo(@PathVariable String codigo){
-        return null;
+        return this.autorService.obterPorCodigo(codigo);
     }
 }
